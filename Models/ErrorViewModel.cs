63da3ea0582d4a367db9574b7fac60be9f7dc446
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-04-23 22:15:00 +0530

    Project Initial Commit
    Create project and install main dependency into the project
    
    01.Identity.EntityFrameworkCore
    02.EntityFrameworkCore
    03.EntityFrameworkCore.Tools
    04.EntityFrameworkCore.SqlServer

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UserApp_Login_and_Registration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
